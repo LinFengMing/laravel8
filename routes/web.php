52dacbe25fc12b53b3e03f751583451c47acff7b
jiro <jiro853141@gmail.com>
2021-04-13 22:58:52 +0800

    cart CRUD

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', 'ProductController');
+Route::resource('carts', 'CartController');
+Route::resource('cart-items', 'CartItemController');
