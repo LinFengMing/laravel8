a1d89aff7f593932620486c2fff642c05efde40b
jiro <jiro853141@gmail.com>
2021-04-18 23:39:49 +0800

    Error Exception

--- app/Http/Services/ShortUrlService.php
+++ app/Http/Services/ShortUrlService.php
@@ -15,22 +15,28 @@ class ShortUrlService
 
     public function makeShortUrl($url)
     {
-        $accessToken = env('URL_ACCESS_Token');
-        $data = [
-            'url' => $url
-        ];
+        try {
+            $accessToken = env('URL_ACCESS_Token');
+            $data = [
+                'url' => $url
+            ];
 
-        $response = $this->client->request(
-            'POST',
-            "http://api.pics.ee/v1/links/?access_token=$accessToken",
-            [
-                'headers' => ['Content-Type' => 'application/json'],
-                'body' => json_encode($data)
-            ]
-        );
+            $response = $this->client->request(
+                'POST',
+                "http://api.pics.ee/v1/links/?access_token=$accessToken",
+                [
+                    'headers' => ['Content-Type' => 'application/json'],
+                    'body' => json_encode($data)
+                ]
+            );
 
-        $contents = $response->getBody()->getContents();
-        $contents = json_decode($contents);
+            $contents = $response->getBody()->getContents();
+            $contents = json_decode($contents);
+        } catch (\Throwable $th) {
+            report($th);
+
+            return $url;
+        }
 
         return $contents->data->picseeUrl;
     }
